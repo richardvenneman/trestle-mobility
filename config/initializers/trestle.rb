d034375fff7fd74ab8ec8fd4f34fd0b28ab30187
Richard Venneman <richardvenneman@me.com>
2019-05-14 16:24:07 -0300

    Cleanup asset names

--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -3,13 +3,8 @@ require "trestle/mobility/fields/text_area"
 require "trestle/mobility/fields/text_field"
 
 Trestle.configure do |config|
-  config.hook(:stylesheets) do
-    stylesheet_link_tag "trestle/mobility"
-  end
-
-  config.hook(:javascripts) do
-    javascript_include_tag "trestle/mobility_fields.js"
-  end
+  config.hook(:stylesheets) { stylesheet_link_tag "trestle/mobility" }
+  config.hook(:javascripts) { javascript_include_tag "trestle/mobility" }
 
   config.form_field :mobility_check_box, Trestle::Mobility::Fields::CheckBox
   config.form_field :mobility_text_area, Trestle::Mobility::Fields::TextArea
--- lib/trestle/mobility/engine.rb
+++ lib/trestle/mobility/engine.rb
@@ -1,7 +1,7 @@
 module Trestle
   module Mobility
     class Engine < Rails::Engine
-      config.assets.precompile << "trestle/mobility_fields.js" << "trestle/mobility.css"
+      config.assets.precompile << "trestle/mobility.css" << "trestle/mobility.js"
     end
   end
 end
